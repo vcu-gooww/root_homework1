dfc4977a8dd544f4d3f553686f76f8bf1c7e0628
vcu-gooww <78395107+vcu-gooww@users.noreply.github.com>
2021-02-15 22:34:18 -0500

    Update hw1.py

--- homework1/hw1.py
+++ homework1/hw1.py
@@ -5,14 +5,14 @@ def return_number_3():
     """ This function should return an integer with the value of 3"""
 
     return_value = 3
-    assert return_value
+    return return_value
 
 
 def return_string_vcu():
     """ This function should return a string with the lowercase value of vcu"""
 
     return_value = "vcu"
-    assert return_value
+    return return_value
 
 
 def return_lowercased_string(input_string):
@@ -20,7 +20,7 @@ def return_lowercased_string(input_string):
     Return it but the lowercase version of it."""
 
     return_value = input_string
-    assert return_value
+    return return_value
 
 
 def return_without_starting_ending_whitespace(input_string):
@@ -28,11 +28,11 @@ def return_without_starting_ending_whitespace(input_string):
     Return it but with the surrounding (left and right) whitespace stripped."""
 
     return_value = input_string
-    assert return_value
+    return return_value
 
 
 def return_addition(first_number, second_number):
     """ Return the two numbers added together. """
 
     return_value = 3
-    assert return_value
+    return return_value
